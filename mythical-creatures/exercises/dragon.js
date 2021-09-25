99f25d3145aa65ec319bb48cd7b0a53773f118b9
Alex Fritz <alexfritzcoach@gmail.comm>
2021-09-25 10:54:14 -0700

    Add dragon-test.js exercise

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,24 @@
+class Dragon {
+  constructor(name, rider, count) {
+    this.name = name;
+    this.rider = rider;
+    this.hungry = true;
+    this.count = 0;
+  }
+  greet() {
+    return `Hi, ${this.rider}!`;
+  }
+  eat(count) {
+    this.count++;
+    if (this.count >= 3){
+      return this.hungry = false;
+    }
+  }
+}
 
+
+module.exports = Dragon;
+// 1. read the test
+// 2. ask yourself if its going to fail and why
+// 3. run the test
+// 4. make it pass
