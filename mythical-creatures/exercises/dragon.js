a5bca7fc9bd51bc08aa3806d3ecf7944fde52640
Alex Fritz <alexfritzcoach@gmail.comm>
2021-10-18 14:45:27 -0700

    Start over exercises for Mod1

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,20 +1,3 @@
-class Dragon {
-  constructor(name, rider, count) {
-    this.name = name;
-    this.rider = rider;
-    this.hungry = true;
-    this.count = 0;
-  }
-  greet() {
-    return `Hi, ${this.rider}!`;
-  }
-  eat(count) {
-    this.count++;
-    if (this.count >= 3){
-      return this.hungry = false;
-    }
-  }
-}
 
 
 module.exports = Dragon;
--- mythical-creatures/exercises/unicorn.js
+++ mythical-creatures/exercises/unicorn.js
@@ -1,21 +1,5 @@
-class Unicorn {
-  constructor(name, color) {
-    this.name = name;
-    if (color === undefined) {
-      this.color = 'white';
-    } else {
-      this.color = color;
-    }
-  }
-  isWhite() {
-    if (this.color !== 'white') {
-      return false
-    }
-  }
-  says(message) {
-    return `**;* ${message} *;**`;
-  }
-}
+
+
 
 module.exports = Unicorn;
 // 1. read the test
