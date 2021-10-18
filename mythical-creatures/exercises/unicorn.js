a9093d66807b9ed61a357172996baafa7caf7b05
Alex Fritz <alexfritzcoach@gmail.comm>
2021-10-18 15:25:02 -0700

    Pass Unicorn tests

--- mythical-creatures/exercises/unicorn.js
+++ mythical-creatures/exercises/unicorn.js
@@ -1,5 +1,24 @@
 
-
+class Unicorn {
+  constructor(name, color) {
+    this.name = name;
+    if (color === undefined) {
+      this.color = `white`;
+    } else {
+      this.color = color;
+    };
+  }
+  isWhite() {
+    if (this.color === `white`) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  says(message) {
+    return `**;* ${message} *;**`;
+  }
+}
 
 module.exports = Unicorn;
 // 1. read the test
