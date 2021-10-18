c0a272ccc2b68dcb9eeb230e731a16b8b9539335
Alex Fritz <alexfritzcoach@gmail.comm>
2021-10-18 15:32:34 -0700

    Pass Dragon test

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,4 +1,20 @@
-
+class Dragon {
+  constructor(name, rider) {
+    this.name = name;
+    this.rider = rider;
+    this.hungry = true;
+    this.count = 0;
+  }
+  greet() {
+    return `Hi, ${this.rider}!`
+  }
+  eat() {
+    this.count++;
+    if (this.count === 3) {
+      this.hungry = false;
+    }
+  }
+}
 
 module.exports = Dragon;
 // 1. read the test
