d6a1794babf3f00d4f47d5e83437421aa491e632
Alex Fritz <alexfritzcoach@gmail.comm>
2021-10-18 21:51:42 -0700

    Pass Medusa test

--- mythical-creatures/exercises/medusa.js
+++ mythical-creatures/exercises/medusa.js
@@ -1,2 +1,27 @@
 var Person = require('./person');
 var Statue = require('./statue');
+
+
+
+class Medusa {
+  constructor(name) {
+    this.name = name;
+    this.statues = [];
+  }
+  gazeAtVictim(victim) {
+    this.statues.push(new Statue(victim.name));
+    if (this.statues.length > 3) {
+      var person = this.statues.shift();
+      return new Person(person.name, `relieved`);
+    }
+  }
+}
+
+
+
+
+
+
+
+
+module.exports = Medusa;
