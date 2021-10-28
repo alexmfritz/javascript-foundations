59a1c9ade644f515ee08f88cd7da83e6bca48d34
Alex Fritz <alexfritzcoach@gmail.comm>
2021-10-28 15:29:32 -0700

    Add Part test

--- oregon-trail/src/part.js
+++ oregon-trail/src/part.js
@@ -1,5 +1,24 @@
-class Part {
 
+
+
+
+class Part {
+  constructor(type) {
+    this.type = type;
+    this.broken = false;
+    this.counter = 0;
+  }
+  break() {
+    this.counter++;
+    this.broken = true;
+  }
+  repair() {
+    if (this.counter < 2) {
+      this.broken = false;
+    } else {
+      return `This ${this.type} has seen better days, we'll need a brand new one!`;
+    }
+  }
 }
 
 module.exports = Part;
