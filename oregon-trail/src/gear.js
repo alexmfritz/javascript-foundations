aafde71dbe784e277840fad31c65099808b45296
Alex Fritz <alexfritzcoach@gmail.comm>
2021-10-28 15:17:07 -0700

    Pass Gear test

--- oregon-trail/src/gear.js
+++ oregon-trail/src/gear.js
@@ -1,5 +1,18 @@
-class Gear {
 
+
+
+class Gear {
+  constructor(gear, amount) {
+    this.type = gear;
+    this.quantity = amount;
+  }
+  checkForValidity() {
+    if ((this.type === 'food') || (this.type ==='ammunition') || (this.type ==='clothes')) {
+      return `Great, we'll need lots of ${this.type}!`;
+    }
+    this.type = null;
+    return `I don't think a mp3 player will help us.`;
+  }
 }
 
 module.exports = Gear;
