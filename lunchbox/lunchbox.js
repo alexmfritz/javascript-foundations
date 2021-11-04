9fc926365d7b0a3635be18cc7e30788edede43e9
Alex Fritz <alexfritzcoach@gmail.comm>
2021-11-04 11:25:10 -0700

    Pass Lunchbox tests

--- lunchbox/lunchbox.js
+++ lunchbox/lunchbox.js
@@ -1,5 +1,27 @@
-class LunchBox {
+var Snack = require('./snack');
 
+
+class LunchBox {
+  constructor(object) {
+    this.owner = object.owner;
+    this.material = object.madeOf
+    this.shape = object.shape;
+    this.color = object.color;
+    this.snacks = [];
+  }
+  acquireSnack(snack) {
+    this.snacks.push(snack);
+    snack.isInLunchBox = true;
+  }
+  findHealthySnacks() {
+    var healthySnacks = [];
+    for(var i = 0; i < this.snacks.length; i++) {
+      if (this.snacks[i].checkForHealthy()) {
+        healthySnacks.push(this.snacks[i].type);
+      }
+    }
+    return healthySnacks;
+  }
 }
 
 module.exports = LunchBox;
--- lunchbox/snack.js
+++ lunchbox/snack.js
@@ -1,5 +1,29 @@
-class Snack {
 
+
+
+class Snack {
+  constructor(snack) {
+    this.deliciousLevel = `extra`;
+    this.type = snack;
+    this.amount = 100;
+    this.isInLunchBox = false;
+
+  }
+  getEaten() {
+    this.amount -= 10;
+    if (this.amount <= 20) {
+      this.cuttingItClose = true;
+    } else {
+      this.cuttingItClose = false;
+    }
+  }
+  checkForHealthy() {
+    if ((this.type.includes(`fruit`)) || (this.type.includes(`Fruit`))) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
 
 module.exports = Snack;
