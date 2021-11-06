75cf96c8a53370f85429a85c65663addb9aa42a2
Alex Fritz <alexfritzcoach@gmail.comm>
2021-11-06 14:36:33 -0700

    Pass Golf-Course tests

--- golf-course/golf-course.js
+++ golf-course/golf-course.js
@@ -1,6 +1,24 @@
+
+
+
 class GolfCourse {
-
-
+  constructor(name, diff, openings, features) {
+    this.name = name;
+    this.difficulty = diff;
+    this.openings = openings;
+    this.features = features;
+    this.currentlyPlaying = [];
+  }
+  checkInGroup(group) {
+    if (group.length <= this.openings) {
+      for(var i = 0; i < group.length; i++) {
+        this.currentlyPlaying.unshift(group[i].name);
+      }
+      this.openings -= group.length;
+      return `You're checked in. Have fun!`;
+    }
+      return `Sorry, we are currently booked! Please come back later.`;
+  }
 }
 
 module.exports = GolfCourse;
--- golf-course/golfer.js
+++ golf-course/golfer.js
@@ -1,6 +1,43 @@
+
+
+
+
 class Golfer {
-
-
+  constructor(golfer) {
+    this.name = golfer.name;
+    this.handicap = golfer.handicap;
+    this.frustration = 0;
+    this.confidence = 0;
+  }
+  calculateAvg(par) {
+    this.handicap += par;
+    return `I usually shoot a ${this.handicap} on average.`;
+  }
+  playRound(course) {
+    if (course.difficulty === `hard`) {
+      this.frustration  = 500;
+    } else {
+      this.frustration = 100;
+    }
+  }
+  hitTheRange() {
+    this.confidence += 10;
+  }
+  marvel(course) {
+    return `I love the ${course.features.join(' and ')} on this course!`;
+  }
+  whatYaShoot(shots) {
+    if (shots < 0) {
+      this.frustration = 0;
+      return `I AM THE GREATEST GOLFER ALIVE!`;
+    } else if (shots === 0) {
+      this.frustration -= 10;
+      return `Booyah!`;
+    } else if (shots > 0) {
+      this.frustration += 20;
+      return `Doh!`;
+    }
+  }
 }
 
 module.exports = Golfer;
