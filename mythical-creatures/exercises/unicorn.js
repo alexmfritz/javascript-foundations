29e40bf967693840c9d48d42fc548ab81faa018f
Alex Fritz <alexfritzcoach@gmail.comm>
2021-11-06 20:50:13 -0700

    Refactor Unicorn test

--- mythical-creatures/exercises/unicorn.js
+++ mythical-creatures/exercises/unicorn.js
@@ -2,26 +2,15 @@
 class Unicorn {
   constructor(name, color) {
     this.name = name;
-    if (color === undefined) {
-      this.color = `white`;
-    } else {
-      this.color = color;
-    };
+    this.color = color || `white`;
   }
   isWhite() {
-    if (this.color === `white`) {
-      return true;
-    } else {
-      return false;
-    }
+    return false;
   }
-  says(message) {
-    return `**;* ${message} *;**`;
+  says(saying) {
+    return `**;* ${saying} *;**`;
   }
 }
 
+
 module.exports = Unicorn;
-// 1. read the test
-// 2. ask yourself if its going to fail and why
-// 3. run the test
-// 4. make it pass
