3dc16aa2017fd10cfa0d08242b4fc05f3b85e4e2
Alex Fritz <alexfritzcoach@gmail.comm>
2021-11-06 20:59:07 -0700

    Refactor Pirate Test

--- mythical-creatures/exercises/pirate.js
+++ mythical-creatures/exercises/pirate.js
@@ -1,33 +1,31 @@
 
+
 class Pirate {
   constructor(name, job) {
     this.name = name;
-    if (job === undefined) {
-      this.job = `scallywag`;
-    } else {
-      this.job = job;
-    }
+    this.job = job || `scallywag`;
     this.cursed = false;
     this.booty = 0;
+    this.count = 0;
   }
   robShip() {
-    if (this.booty < 500) {
-      this.booty += 100;
-      return `YAARRR!`;
-    } else {
+    if (this.count === 5) {
       this.cursed = true;
-      return `ARG! I\'ve been cursed!`;
+      return `ARG! I've been cursed!`;
     }
+    this.count++;
+    this.booty += 100;
+    return `YAARRR!`;
   }
   liftCurse() {
     if (this.cursed === true) {
-      this.booty = 200;
+      this.booty -= 300;
       this.cursed = false;
       return `Your curse has been lifted!`;
-    } else {
-      return `You don\'t need to lift a curse!`;
     }
+    return `You don't need to lift a curse!`
   }
 }
+
 
 module.exports = Pirate;
