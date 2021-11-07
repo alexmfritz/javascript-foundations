e646106b8163e493085d8c50ce714878fda700ed
Alex Fritz <alexfritzcoach@gmail.comm>
2021-11-06 20:49:34 -0700

    Pass Roadrace tests

--- roadrace/src/roadrace.js
+++ roadrace/src/roadrace.js
@@ -1,5 +1,25 @@
-class Roadrace {
 
+
+
+class Roadrace {
+  constructor(race) {
+    this.name = race.title;
+    this.location = race.city;
+    this.distance = 0;
+    this.participants = [];
+  }
+  setDistance(miles) {
+    this.distance += miles;
+    return `The ${this.name} in ${this.location} is a ${this.distance} mile race.`;
+  }
+  registerParticipants(racer) {
+    this.participants.push(racer);
+  }
+  completeRace() {
+    for(var i = 0; i < this.participants.length; i++) {
+      this.participants[i].runRace(this.name, this.distance);
+    }
+  }
 }
 
 module.exports = Roadrace;
--- roadrace/src/runner.js
+++ roadrace/src/runner.js
@@ -1,5 +1,26 @@
-class Runner {
 
+
+
+class Runner {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+    this.milesRun = 0;
+    this.fitness = 25;
+    this.completedRaces = [];
+  }
+  runSomeMiles(miles) {
+    this.milesRun += miles;
+    this.fitness += (1 * miles);
+  }
+  stretch() {
+    this.fitness += .5;
+  }
+  runRace(race, miles) {
+    this.completedRaces.push(race);
+    this.milesRun += miles;
+    this.fitness += miles;
+  }
 }
 
 module.exports = Runner;
