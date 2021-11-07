a88fe74d7bc59911dffce6a1b59edf77748754e8
Alex Fritz <alexfritzcoach@gmail.comm>
2021-11-06 21:28:02 -0700

    Pass Crafting Test

--- crafting/src/craft.js
+++ crafting/src/craft.js
@@ -1,5 +1,22 @@
-class Craft {
 
+
+class Craft {
+  constructor(craft) {
+    this.name = craft.type;
+    this.materials = craft.materials;
+    this.completed = false;
+  }
+  completeCraft() {
+    this.completed = true;
+    return `All done! It looks great!`;
+  }
+  calculateProjectTotal() {
+    var total = 0;
+    for(var i = 0; i < this.materials.length; i ++) {
+      total += this.materials[i].price * this.materials[i].amount;
+    }
+    return total;
+  }
 }
 
 module.exports = Craft;
--- crafting/src/material.js
+++ crafting/src/material.js
@@ -1,5 +1,20 @@
 class Material {
-
+  constructor(name, price, amt, units) {
+    this.name = name;
+    this.price = price;
+    this.amount = amt;
+    this.units = units;
+  }
+  useMaterial(amt) {
+    if (amt > this.amount) {
+      return `You don't have enough ${this.name}! Try using ${this.amount} ${this.units} or less.`;
+    }
+    this.amount -= amt;
+    return `You now have ${this.amount} ${this.units} of ${this.name} left.`;
+  }
+  calculateMaterialCost() {
+    return this.price *= this.amount;
+  }
 }
 
 module.exports = Material;
