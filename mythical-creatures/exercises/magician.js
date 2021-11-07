6d31c526a4e0d8ea7572afdf1d3eed3d211bb5c7
Alex Fritz <alexfritzcoach@gmail.comm>
2021-11-06 20:58:01 -0700

    Refactor Magician test

--- mythical-creatures/exercises/magician.js
+++ mythical-creatures/exercises/magician.js
@@ -1,30 +1,29 @@
 
 class Magician {
-  constructor(name) {
-    this.name = `The Great ${name.name}`;
-    this.assistant = name.assistant;
-    this.favoriteAccessory = name.clothing || `top hat`;
+  constructor(magician) {
+    this.name = `The Great ${magician.name}`;
+    this.assistant = magician.assistant;
+    this.favoriteAccessory = magician.clothing || `top hat`;
     this.confidencePercentage = 10;
   }
-  performIncantation(incantation) {
-
-    return (`${incantation.toUpperCase()}!`);
+  performIncantation(spell) {
+    return `${spell.toUpperCase()}!`;
   }
   performTrick() {
     this.confidencePercentage += 10;
     if (this.favoriteAccessory === `top hat`) {
       return `PULL RABBIT FROM TOP HAT`;
-    } else {
-      return `PULL DOVE FROM SLEEVE`;
     }
+    return `PULL DOVE FROM SLEEVE`;
   }
   performShowStopper() {
-    if ((this.confidencePercentage >= 100) && (this.assistant === true)) {
+    if ((this.assistant === true) && (this.confidencePercentage >= 100)) {
       return `WOW! The magician totally just sawed that person in half!`;
-    } else {
-      return `Oh no, this trick is not ready!`;
     }
+    return `Oh no, this trick is not ready!`
   }
 }
+
+
 
 module.exports = Magician;
